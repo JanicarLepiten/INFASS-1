f5e8ee542ea63f59d9fdf246304a351a4e09def6
ACT-STUDENT <ACT-STUDENT@LAB1-PC6>
2026-07-07 07:31:10 +0800

    Add project files.

---
+++ INFASS_LEPITEN/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INFASS_LEPITEN.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
