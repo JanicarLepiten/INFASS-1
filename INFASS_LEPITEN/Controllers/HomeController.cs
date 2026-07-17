1114364af04cd2fac17c9e5087de42fae8dce5b2
Janicar Lepiten <janicarlepiten2124@gmail.com>
2026-07-17 10:34:28 +0800

    Added alert and updated registration

--- INFASS_LEPITEN/Controllers/HomeController.cs
+++ INFASS_LEPITEN/Controllers/HomeController.cs
@@ -28,6 +28,43 @@ namespace INFASS_LEPITEN.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Register(UserModel user)
+        {
+            string message =
+                $"Full Name: {user.FullName}\n" +
+                $"Email: {user.Email}\n" +
+                $"Age: {user.Age}\n" +
+                $"Gender: {user.Gender}\n" +
+                $"Username: {user.Username}\n" +
+                $"Password: {user.Password}\n" +
+                $"Confirm Password: {user.ConfirmPassword}";
+
+            return Json(message);
+        }
+
+        //[HttpPost]
+        //public IActionResult Register(
+        //   string fullName,
+        //   string email,
+        //   int age,
+        //   string gender,
+        //   string username,
+        //   string password,
+        //   string confirmPassword)
+        //{
+        //    string result =
+        //        $"Full Name: {fullName}\n" +
+        //        $"Email: {email}\n" +
+        //        $"Age: {age}\n" +
+        //        $"Gender: {gender}\n" +
+        //        $"Username: {username}\n" +
+        //        $"Password: {password}\n" +
+        //        $"Confirm Password: {confirmPassword}";
+
+        //    return Content(result);
+        //}
+
         public IActionResult Privacy()
         {
             return View();
---
+++ INFASS_LEPITEN/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿namespace INFASS_LEPITEN.Models
+{
+    public class UserModel
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
